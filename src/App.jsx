a26746c8d2717be4979bfa3a573f4b9fbbd27cc3
eha49 <habron.rasheed7@gmail.com>
2024-05-28 09:53:04 +0530

    initialize and scaffolded

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import GlobalStyles from "./components/GlobalStyles/GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <p>This is a paragraph.</p>
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
---
+++ src/constant.js
@@ -0,0 +1,13 @@
+// Choosing the Desktop-First approach
+
+export const BREAKPOINTS = {
+  phone: 600,
+  tablet: 950,
+  laptop: 1300,
+};
+
+export const QUERIES = {
+  phoneAndSmaller: `(max-width: ${BREAKPOINTS.phone / 16}rem)`,
+  tabletAndSmaller: `(max-width: ${BREAKPOINTS.tablet / 16}rem)`,
+  laptopAndSmaller: `(max-width: ${BREAKPOINTS.laptop / 16}rem)`,
+};
